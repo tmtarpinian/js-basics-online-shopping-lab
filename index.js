ec5d5a8ef0c27f28ca8e06ef89095b4be510ae4f
ferrisbueller66 <tmtarpinian@gmail.com>
2020-10-28 22:49:13 -0400

    write total function

--- index.js
+++ index.js
@@ -21,10 +21,38 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  const cart = getCart()
+  let str = "In your cart, you have"
+  if (cart.length <= 0){
+    return `Your shopping cart is empty.`
+  }
+
+  if (cart.length == 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+
+  if (cart.length == 2){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  }
+
+  // if (cart.length > 2{
+  //   return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  // }
+    
+  // 1 item — `In your cart, you have bananas at $17.`
+  // 2 items — `In your cart, you have bananas at $17, and pancake batter at $5.`
+  // 3+ items — `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
+
+//`In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
+
 }
 
 function total() {
   // write your code here
+  let totalValue = 0
+  cart.forEach(i => totalValue += i.itemPrice)
+  return totalValue
+
 }
 
 function removeFromCart(item) {
